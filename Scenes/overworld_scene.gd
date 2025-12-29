d7dcff1c3b67df9ddd75e3c0fbc5d4b0d44db338
a-sriel <rilescentauri@gmail.com>
2025-12-29 01:54:36 -0600

    Begin work on cutscene

--- Scenes/overworld_scene.gd
+++ Scenes/overworld_scene.gd
@@ -2,11 +2,22 @@ extends Node3D
 
 var cave_path : String = "res://Scenes/Cave Scene.tscn"
 
+func _ready() -> void:
+	$CinematicCamera.make_current()
+	$Player.hide()
+	
+	var anim1 : Animation = $ProtagFerret_Idle/ferret/AnimationPlayer.get_animation("Idle")
+	anim1.loop_mode = (Animation.LOOP_LINEAR)
+	$ProtagFerret_Idle/ferret/AnimationPlayer.play("Idle")
+	
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	$Player/Head/Camera3D.make_current()
+
 func _on_enterance_trigger_body_entered(body: Node3D) -> void:
 	if body is CakeMaster:
 		get_tree().change_scene_to_file(cave_path)
 
-func _unhandled_input(_event: InputEvent) -> void:
+func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_key_pressed(KEY_ESCAPE):
 		get_tree().paused = true
 		$Pause_Menu.show()
